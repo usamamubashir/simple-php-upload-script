f8e85c9a00d7357c7171df793cd2eb9811f25a6d
Usama Mubashir <usama918@gmail.com>
2016-05-25 17:31:27 -0400

    Added two checks which it preforms on the files being uploaded. One: checks if the file already exists in the upload dir. Two: checks the file size.

--- upload.php
+++ upload.php
@@ -8,18 +8,35 @@ if ($_FILES['upload']['name'] && isset($_POST['submit'])) {
 	
 	//loop through each file
 	for ($i=0; $i<$total; $i++) {
+                $uploadCheck = 1;
 		//create the target file dir
 		$upload_file = $upload_dir . basename($_FILES['upload']['name'][$i]);
+                //checks if file already exists
+                if (file_exists($upload_file)) {
+                    echo ("The file <font color='blue'>".basename($_FILES['upload']['name'][$i])."</font> already exits.<br>");
+                    $uploadCheck = 0;
+                }
+                //checks the file size, in this case it checks if its more than 5GB.
+                if ($_FILES['upload']['size'][$i] > 5000000000) {
+                    echo ("The file <font color='green'>".basename($_FILES['upload']['name'][$i])."</font> size is too big.<br>");
+                    $uploadCheck = 0;
+                }
+                //checks if $uploadCheck encountered and errors
+                if ($uploadCheck == 0) {
+                    echo ("The file <font color='red'>".basename($_FILES['upload']['name'][$i])."</font> was not uploaded.<br>");
+                    $uploadCheck = 1;
+                } else {
 		//uploads the files into the target dir
 		if (move_uploaded_file($_FILES['upload']['tmp_name'][$i], $upload_file)) {
-			echo ("The file <font color='red'>". basename( $_FILES['upload']['name'][$i])."</font> has been uploaded.<br>");			
+			echo ("The file <font color='green'>". basename( $_FILES['upload']['name'][$i])."</font> has been uploaded.<br>");
 		} else {
-			echo ("There was an error uploading the file.");
+			echo ("There was an error uploading the file.<br>");
 		}
+                }
 	}
 } else {
 	echo ("You must select the files you would like to upload.");
 }
 
 ?>
-<a href="upload.html"> << back </a>
+<br><a href="upload.html"> << back </a>
